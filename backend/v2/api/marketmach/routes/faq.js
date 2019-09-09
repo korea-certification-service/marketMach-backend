2a37ed38783c902fa3dee78e4d4b8e2e8d9dc82a
dongil Kim <kdi@onlykcs.com>
2019-09-09 15:40:07 +0900

    - faq 버그 수정

--- backend/v2/api/marketmach/routes/faq.js
+++ backend/v2/api/marketmach/routes/faq.js
@@ -66,6 +66,7 @@ router.get('/list', function (req, res, next) {
 });
 
 router.get('/detail/:faqId', function (req, res, next) {
+    let country = dbconfig.country;
     let bitwebResponse = new BitwebResponse();
     let condition = {
         "_id" : req.params.faqId
