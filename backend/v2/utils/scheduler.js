fa22f478f610ee0b0112b40ef74cc284382e0103
dongil Kim <kdi@onlykcs.com>
2019-09-24 16:43:50 +0900

    - 주석 처리

--- backend/v2/utils/scheduler.js
+++ backend/v2/utils/scheduler.js
@@ -40,7 +40,7 @@ function ontJob(data) {
                     if (jobCount == endJobCount) {
                         console.log('End jobCount==>', jobCount);
                         //등록된 coinHistory 삭제
-                        serviceCoinHistorys.remove(country,{"_id": historyId});
+                        // serviceCoinHistorys.remove(country,{"_id": historyId});
                         job.cancel();
                         return;
                     }
