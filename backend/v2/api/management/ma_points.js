de13822770cc6fa95049d1ddd21709a35cb42f4e
dongil Kim <kdi@onlykcs.com>
2019-11-15 14:18:22 +0900

    - 포인트 거래 내역 조회 API 추가

--- backend/v2/api/management/ma_points.js
+++ backend/v2/api/management/ma_points.js
@@ -3,7 +3,10 @@ var router = express.Router();
 let dbconfig = require('../../../../config/dbconfig');
 let BitwebResponse = require('../../utils/BitwebResponse');
 let servicePoints = require('../../service/points');
-
+let servicePointTrades = require('../../service/pointTrades');
+let serviceVtrTemps = require('../../service/vtrTemps');
+let logger = require('../../utils/log');
+let token = require('../../utils/token');
 
 /*GET Points Detail*/
 router.get("/detail/:pointId", (req, res) => {
@@ -47,5 +50,56 @@ router.put("/modify/:pointId", (req, res) => {
         res.status(500).send(bitwebResponse.create())
     })
 });
+
+router.post("/list/all", token.checkInternalToken, async (req, res) => {
+    let bitwebResponse = new BitwebResponse();
+    let country = dbconfig.country;
+    let condition = req.body.param;
+    if(req.body.param.country == "KR") {
+        condition['$or'] = [{country:{$exists:false}},{country:"KR"}];        
+    }    
+    let option = req.body.option;
+
+    try {
+        let count = await servicePointTrades.count(country, condition, option);
+        let list = await servicePointTrades.list(country, condition, option);
+
+        for(var i in list) {
+            let tempCondition = {
+                "item._id": list[i]._doc.item._id
+            }
+            let tempResult = await serviceVtrTemps.detail(country, tempCondition);
+            if(tempResult != null) {
+                delete tempResult._doc['item'];                
+            }
+            list[i]._doc['vtrTemp'] = tempResult;
+        }
+
+        bitwebResponse.code = 200;
+        let resData = {
+            "successYn": "Y",
+            "count": count,
+            "list": list
+        }
+        //API 처리 결과 별도 LOG로 남김
+        logger.addLog(country, req.originalUrl, req.body, resData);
+        bitwebResponse.data = resData;
+
+        let jsonResult = bitwebResponse.create();
+
+        jsonResult['pageIdx'] = option.pageIdx;
+        jsonResult['perPage'] = option.perPage;
+
+        res.status(200).send(jsonResult);
+    } catch(err) {
+        let resErr = "처리중 에러 발생";
+        //API 처리 결과 별도 LOG로 남김
+        logger.addLog(country, req.originalUrl, condition, err.message);
+
+        bitwebResponse.code = 500;
+        bitwebResponse.message = resErr;
+        res.status(500).send(bitwebResponse.create())
+    }
+})
 
 module.exports = router; 
